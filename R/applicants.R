e465024e3d5b0b227028068f32c8317997292445
= <=>
2024-09-25 09:57:43 +0200

    The function winsorized_mean is added The data applicants is added

---
+++ R/applicants.R
@@ -0,0 +1,19 @@
+#' Applicants
+#'
+#' A subset of data from the paper of LLamazares
+#'
+#'
+#' @format ## `applicants`
+#' A data frame with 3 rows and 8 columns:
+#' \describe{
+#'   \item{Student}{ A letter to indentify the person}
+#'   \item{MatI}{Mark in Mathematics I}
+#'   \item{MatII}{Mark in Mathematics II}
+#'   \item{MatIII}{Mark in Mathematics III}
+#'   \item{StatI}{Mark in Statistics I}
+#'   \item{StatII}{Mark in Statistics II}
+#'   \item{EcoI}{Mark in Economics I}
+#'   \item{EcoII}{Mark in Economics II}
+#' }
+#' @source Paper
+"applicants"
